9829bf4c08b25bc70817ce657acdd2cc631509ff
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-07-01 18:10:38 +0300

    connections experiment

--- db/db.go
+++ db/db.go
@@ -11,6 +11,8 @@ func DBConnect() *sqlx.DB {
 	// connect_timeout ./vendor/github.com/lib/pq/doc.go in seconds
 	// statement_timeout https://postgrespro.ru/docs/postgrespro/9.6/runtime-config-client
 	db, err := sqlx.Connect("postgres", "host=172.24.0.2 user=postgres password=postgresqlPassword dbname=postgres connect_timeout=2 statement_timeout=2000 sslmode=disable")
+	db.SetMaxOpenConns(500)
+	db.SetMaxIdleConns(500)
 	if err != nil {
 		log.Panic(err)
 	}
