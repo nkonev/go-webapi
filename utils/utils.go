479d58a0460f8ca8e6b5fcc387b17302435250db
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-10-17 02:42:12 +0300

    add todo

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,7 @@ import "golang.org/x/crypto/bcrypt"
 
 type H map[string]interface{}
 
+// TODO salt
 func HashPassword(password string) (string, error) {
 	passwordHash, passwordHashErr := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if passwordHashErr != nil {
